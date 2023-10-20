b06e4abde4707d65d52afce9198e7d9593d36a2c
Prateek <56234509+pxc-dev@users.noreply.github.com>
2023-10-20 16:54:54 -0700

    Fix typo in variable desc

--- variables.tf
+++ variables.tf
@@ -35,7 +35,7 @@ variable "generate_key" {
 
     Note that this will persist a key in the remote state used by Terraform. Please take appropriate measures to protect your remote state.
 
-    If not set to 'true' you will to create a JSON key via another mechanism (e.g. GCP console) before registration.
+    If not set to 'true' you will need to create a JSON key via another mechanism (e.g. GCP console) before registration.
   EOF
   type        = bool
 }
